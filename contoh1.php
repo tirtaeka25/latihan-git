5398b18475b68abc82b934654ea6557b4a527c2f
tirtaeka25 <tirtaeka1998@gmail.com>
2023-09-26 18:23:22 +0700

    add latihan1.php contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+class Contoh1 extends CI_Controller
+{
+    Public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Tirta Eka Saputra
+        saya tinngal di daerah sempu
+        olahraga yang saya sukai adalah Futsal";
+    }
+}
