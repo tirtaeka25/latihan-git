f243a0df79a10a70d0152669ede919668f7e22d4
tirtaeka25 <133229830+tirtaeka25@users.noreply.github.com>
2023-10-08 15:19:05 +0700

    Add files via upload

--- Latihan1.php
+++ Latihan1.php
@@ -1,15 +1,23 @@
-<?php
+<?php 
+class Matakuliah extends CI_Controller 
+{ 
+ 
+    public function index() 
 
-class Latihan1 extends CI_Controller
-{
-    Public function index()
-    {
-        echo "<h1>Biodata</h1>";
-        echo"Nama                  : Tirta Eka Saputra<p>
-        NIM                   : 12220200<p>
-        Kelas                 : 12.2B.24<p>
-        Hobby                 : Futsal, Badminton<p>
-        Tempat tinggal        : Sempu Rt.3/Rw.3<p>
-        Tempat, tanggal lahir : Bandung, 25 Oktober 1998";
-    }
-}
+    { 
+ 
+        $this->load->view('view-form-matakuliah'); 
+    } 
+ 
+    public function cetak() 
+    { 
+        
+        $data = [ 
+            'kode' => $this->input->post('kode'), 
+            'nama' => $this->input->post('nama'), 
+            'sks' => $this->input->post('sks') 
+        ]; 
+ 
+        $this->load->view('view-data-matakuliah', $data); 
+    } 
+} 
